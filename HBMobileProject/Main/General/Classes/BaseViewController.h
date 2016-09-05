957ab489da6ef2625b3df856a0041f46254fe12f
HarbingWang <1792427320@qq.com>
2016-09-05 10:39:16 +0800

    项目架构

---
+++ HBMobileProject/Main/General/Classes/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  HBMobileProject
+//
+//  Created by HarbingWang on 16/9/5.
+//  Copyright © 2016年 HarbingWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UITableViewController
+
+@end
---
+++ HBMobileProject/Main/General/Classes/BaseViewController.m
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.m
+//  HBMobileProject
+//
+//  Created by HarbingWang on 16/9/5.
+//  Copyright © 2016年 HarbingWang. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@implementation BaseViewController
+
+@end
