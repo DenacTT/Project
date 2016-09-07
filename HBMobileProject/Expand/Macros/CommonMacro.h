36811791008c7d8943d59815f7c7c8a1fe29e52f
HarbingWang <1792427320@qq.com>
2016-09-07 15:11:19 +0800

    pch 文件, Debug模式打印日志

--- HBMobileProject/Expand/Macros/CommonMacro.h
+++ HBMobileProject/Expand/Macros/CommonMacro.h
@@ -20,13 +20,13 @@
 #define ScreenHeight [UIScreen mainScreen].bounds.size.height
 
 // String字符串
-#define STR(key)            NSLocalizedString(key, nil)
+#define STR(key) NSLocalizedString(key, nil)
 
 //字体
-#define Font(fontSize)         [UIFont systemFontOfSize:fontSize]
+#define Font(fontSize) [UIFont systemFontOfSize:fontSize]
 
 //图片
-#define Image(imageName)       [UIImage imageNamed:imageName]
+#define Image(imageName) [UIImage imageNamed:imageName]
 
 // Color
 #define RGBA(r,g,b,a) [UIColor colorWithRed:r/255.0f green:g/255.0f blue:b/255.0f alpha:a]
@@ -34,13 +34,27 @@
 #define RandomColor RGB(arc4random_uniform(256), arc4random_uniform(256), arc4random_uniform(256))
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
-/* --------------  DEBUG  模式下打印日志,当前行  -----------*/
-#define NSLog(format, ...) do {                                                                                                         \
-fprintf(stderr, "<%s : %d> %s\n",                                                                                                      \
-[[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String],                     \
-__LINE__, __func__);                                                                                                                       \
-(NSLog)((format), ##__VA_ARGS__);                                                                                            \
-fprintf(stderr, "------------------------------------------------------\n");        \
-} while (0)
+/* --------------  打印日志   -------------*/
+//#define NSLog(format, ...) do {                                                                                                         \
+//fprintf(stderr, "<%s : %d> %s\n",                                                                                                      \
+//[[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String],                     \
+//__LINE__, __func__);                                                                                                                       \
+//(NSLog)((format), ##__VA_ARGS__);                                                                                            \
+//fprintf(stderr, "------------------------------------------------------\n");        \
+//} while (0)
+
+// 当前是否debug模式
+#ifdef DEBUG
+#  define isDebug 1
+#else
+#  define isDebug 0
+#endif
+
+//重写NSLog,Debug模式下打印日志和当前行数
+#if DEBUG
+#define NSLog(FORMAT, ...) fprintf(stderr,"\nfunction:%s line:%d content:%s\n", __FUNCTION__, __LINE__, [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String]);
+#else
+#define NSLog(FORMAT, ...) nil
+#endif
 
 #endif /* CommonMacro_h */
