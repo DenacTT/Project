0091baf628e2a2673593032a65b846458e1f24b4
HarbingWang <1792427320@qq.com>
2016-09-09 22:25:10 +0800

    UIControl ClickDelay

---
+++ HBMobileProject/Expand/Category/UIControl/UIControl+ClickDelay.h
@@ -0,0 +1,13 @@
+//
+//  UIControl+ClickDelay.h
+//  HBMobileProject
+//
+//  Created by HarbingWang on 16/9/9.
+//  Copyright © 2016年 HarbingWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIControl (ClickDelay)
+
+@end
---
+++ HBMobileProject/Expand/Category/UIControl/UIControl+ClickDelay.m
@@ -0,0 +1,13 @@
+//
+//  UIControl+ClickDelay.m
+//  HBMobileProject
+//
+//  Created by HarbingWang on 16/9/9.
+//  Copyright © 2016年 HarbingWang. All rights reserved.
+//
+
+#import "UIControl+ClickDelay.h"
+
+@implementation UIControl (ClickDelay)
+
+@end
