58d3f5c5c86241d9c38fca12083db7562513fa10
HarbingWang <1792427320@qq.com>
2016-09-09 11:18:21 +0800

    修正图片方向

--- HBMobileProject/Expand/Category/UIImageCategory/UIImage+RoundedCorner.h
+++ HBMobileProject/Expand/Category/UIImageCategory/UIImage+RoundedCorner.h
@@ -10,9 +10,17 @@
 
 @interface UIImage (RoundedCorner)
 
-// 为图片切圆角
-// 避免在大量cell离屏渲染的时候拖慢fps
+/**
+ *  @author HarbingWang, 16-09-09 10:09:36
+ *
+ *  为图片切圆角
+ *  避免在大量cell离屏渲染的时候拖慢fps
+ *
+ *  @param sizeToFit <#sizeToFit description#>
+ *  @param radius    <#radius description#>
+ *
+ *  @return <#return value description#>
+ */
 -(UIImage *)imageWithRoundedCornersAndSize:(CGSize)sizeToFit andCornerRadius:(CGFloat)radius;
-
 
 @end
---
+++ HBMobileProject/Expand/Category/UIImageCategory/UIImage+fixOrientation.h
@@ -0,0 +1,20 @@
+//
+//  UIImage+fixOrientation.h
+//  HBMobileProject
+//
+//  Created by HarbingWang on 16/9/9.
+//  Copyright © 2016年 HarbingWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (fixOrientation)
+
+/**
+ *  修正图片方向
+ *
+ *  @return 修改后的图片
+ */
+- (UIImage *)fixOrientation;
+
+@end
