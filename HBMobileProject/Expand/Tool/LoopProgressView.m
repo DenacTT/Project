a7d8f2667a3bd9912a87df08f658173650a6da8a
HarbingWang <1792427320@qq.com>
2016-09-09 21:58:49 +0800

    LoopProgressView

---
+++ HBMobileProject/Expand/Tool/LoopProgressView.m
@@ -0,0 +1,25 @@
+//
+//  LoopProgressView.m
+//  HBMobileProject
+//
+//  Created by HarbingWang on 16/9/9.
+//  Copyright © 2016年 HarbingWang. All rights reserved.
+//  圆环形进度条(进度条 数字同步) http://www.cnblogs.com/fcug/p/5195522.html
+
+#import "LoopProgressView.h"
+#import <QuartzCore/QuartzCore.h>
+
+@interface LoopProgressView ()
+
+{
+    CAShapeLayer *archLayer;
+    UILabel *label;
+    NSTimer *progressTimer;
+}
+@end
+
+@implementation LoopProgressView
+
+
+
+@end
