7fb783a149dde6343e5e4cf117eda1b1322217f1
HarbingWang <1792427320@qq.com>
2016-09-12 21:34:59 +0800

    tableView

--- HBMobileProject/Main/PublishCenter/Controller/UploadMovieViewController.m
+++ HBMobileProject/Main/PublishCenter/Controller/UploadMovieViewController.m
@@ -8,7 +8,11 @@
 
 #import "UploadMovieViewController.h"
 
-@interface UploadMovieViewController ()
+#define UploadMovieCellID @"UploadMovieCell"
+
+@interface UploadMovieViewController ()<UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *tableView;
 
 @end
 
@@ -19,16 +23,47 @@
     
     self.title = @"上传";
     
-    self.view.backgroundColor = [UIColor lightGrayColor];
-    
-    
+    [self.view addSubview:_tableView];
 }
 
-- (void)viewDidAppear:(BOOL)animated
+#pragma mark - UITableViewDelegate
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    
+    return 100;
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"%zi", indexPath.item);
+}
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UploadMovieCell *cell = [tableView dequeueReusableCellWithIdentifier:UploadMovieCellID forIndexPath:indexPath];
+    if (!cell) {
+        
+    }
+//    cell.delegate
+    return cell;
+}
+
+#pragma mark - LazyLoad
+- (UITableView *)tableView
+{
+    if (!_tableView) {
+        _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 64, ScreenWidth, ScreenHeight - 64) style:(UITableViewStylePlain)];
+        _tableView.delegate  = self;
+        _tableView.dataSource = self;
+        [_tableView registerClass:[UploadMovieCell class] forCellReuseIdentifier:UploadMovieCellID];
+    }
+    return _tableView;
+}
 
 - (void)didReceiveMemoryWarning
 {
