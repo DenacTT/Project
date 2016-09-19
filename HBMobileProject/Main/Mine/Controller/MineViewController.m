a455ab6fa992a2ce1d8acbc6a9992da0e6b76b42
HarbingWang <1792427320@qq.com>
2016-09-19 14:49:20 +0800

    MineViewController

--- HBMobileProject/Main/Mine/Controller/MineViewController.m
+++ HBMobileProject/Main/Mine/Controller/MineViewController.m
@@ -8,6 +8,73 @@
 
 #import "MineViewController.h"
 
+#define TableViewCellID @"cellID"
+
+@interface MineViewController()<UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) NSMutableArray *dataSource;
+@property (nonatomic, strong) UITableView *tableView;
+
+@end
+
 @implementation MineViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.titleLabel.text = @"功能";
+    self.isUseBackBtn = YES;
+    self.isUseRightBtn = YES;
+    [self.view addSubview:self.tableView];
+ 
+}
+
+#pragma mark - UITableViewDelegate
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 30.f;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+}
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.dataSource count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:TableViewCellID forIndexPath:indexPath];
+    cell.accessoryType    = UITableViewCellAccessoryDisclosureIndicator;
+    cell.textLabel.text = self.dataSource[indexPath.row];
+    return cell;
+}
+
+#pragma mark - LazyLoad
+- (NSMutableArray *)dataSource
+{
+    if (!_dataSource) {
+        _dataSource = [NSMutableArray array];
+    }
+    return _dataSource;
+}
+
+- (UITableView *)tableView
+{
+    if (!_tableView) {
+        _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, ScreenWidth, ScreenHeight) style:(UITableViewStylePlain)];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        _tableView.showsVerticalScrollIndicator = NO;
+        
+        [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:TableViewCellID];
+    }
+    return _tableView;
+}
 
 @end
