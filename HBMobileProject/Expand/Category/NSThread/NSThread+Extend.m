b3cf28c8e428b067309b7f01079f394253a96375
HarbingWang <1792427320@qq.com>
2016-09-20 20:00:02 +0800

    NSThread+Extend

--- HBMobileProject/Expand/Category/NSThread/NSThread+Extend.m
+++ HBMobileProject/Expand/Category/NSThread/NSThread+Extend.m
@@ -12,22 +12,35 @@
 
 + (void)runInMain:(void (^)(void))func
 {
+    if ([NSThread isMainThread]) {
+        func();
+        return;
+    }
     
+    dispatch_async(dispatch_get_main_queue(), ^{
+        func();
+    });
 }
 
 + (void)runAfter:(void (^)(void))func second:(float)second
 {
-
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(second * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        func();
+    });
 }
 
 + (void)runInBackground:(void (^)(void))func
 {
-
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        func();
+    });
 }
 
 + (void)runInBackground:(void (^)(void))func afterSecond:(float)second
 {
-    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(second * NSEC_PER_SEC)), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        func();
+    });
 }
 
 @end
