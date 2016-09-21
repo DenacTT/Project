c0950ede4bc1b697e77934db0c7a824862347e17
HarbingWang <1792427320@qq.com>
2016-09-21 22:02:32 +0800

    a

--- HBMobileProject/Expand/Tool/CustomTipsView.m
+++ HBMobileProject/Expand/Tool/CustomTipsView.m
@@ -97,7 +97,47 @@
     } completion:^(BOOL finished) {
         [weakSelf removeFromSuperview];
     }];
-    
 }
 
+/*
+//tips小窗口提示
++ (void)showSuccessTitle:(NSString *)title Top:(CGFloat)top
+{
+    UIView * tipsView = [UIView new];
+    [myWindow() addSubview: tipsView];
+    UIFont * font = [UIFont systemFontOfSize: 16.f];
+    CGSize size = [title YMSizeWithFont: font constrainedToSize: CGSizeMake( ScreenWidth, font.height)];
+    tipsView.width = size.width + 32.f;
+    //    tipsView.height = size.height + 20;
+    tipsView.height = 40.f;
+    tipsView.left = (ScreenWidth - tipsView.width)/2;
+    tipsView.top = (top - tipsView.height)/2;
+    [tipsView addCornerRadius: 5.f];
+    tipsView.backgroundColor = RGBA(0, 0, 0, 0.8f);
+    UILabel * labelTips = [[UILabel alloc]initWithFrame: tipsView.bounds];
+    [tipsView addSubview: labelTips];
+    labelTips.textAlignment = NSTextAlignmentCenter;
+    labelTips.font = font;
+    labelTips.text = title;
+    labelTips.textColor = [UIColor whiteColor];
+    __weak UIView* weakView = tipsView;
+    [NSThread runInMain:^{
+        [UIView animateWithDuration: 0.8f
+                              delay: 1.0f
+                            options: UIViewAnimationOptionBeginFromCurrentState|UIViewAnimationOptionCurveEaseIn
+                         animations:^{
+                             weakView.alpha = 1.f;
+                         } completion: ^(BOOL finished){
+                             {
+                                 [UIView animateWithDuration: 0.5f delay: 1.f options: UIViewAnimationOptionCurveEaseOut animations:^{
+                                     weakView.alpha = 0.f;
+                                 } completion: ^(BOOL finished)
+                                  {
+                                      [weakView removeFromSuperview];
+                                  }];
+                             }
+                         }];
+    }];
+}
+*/
 @end
