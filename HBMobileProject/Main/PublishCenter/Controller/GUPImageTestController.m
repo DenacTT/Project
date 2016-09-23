7b254ac4ce64feb4eba3da473b271b3b7989819b
HarbingWang <1792427320@qq.com>
2016-09-23 14:47:46 +0800

    GUPImageTestController

--- HBMobileProject/Main/PublishCenter/Controller/GUPImageTestController.m
+++ HBMobileProject/Main/PublishCenter/Controller/GUPImageTestController.m
@@ -16,20 +16,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.navigationController.navigationBar setHidden:YES];
     
     UIImage *inputImage = [UIImage imageNamed:@"test"];
     
+    // 获取图像源
     GPUImagePicture *stillImageSource = [[GPUImagePicture alloc] initWithImage:inputImage];
-    GPUImageSepiaFilter *stillImageFilter = [[GPUImageSepiaFilter alloc] init];
     
-    [stillImageSource addTarget:stillImageFilter];
-    [stillImageFilter useNextFrameForImageCapture];
+    /* 褐色（怀旧）滤镜 */
+//    GPUImageSepiaFilter *stillImageFilter = [[GPUImageSepiaFilter alloc] init];
+//    [stillImageSource addTarget:stillImageFilter];
+//    [stillImageFilter useNextFrameForImageCapture];
+//    [stillImageSource processImage];
+    
+    /* 晕影，形成黑色边缘，突出中间图像的效果 */
+    GPUImageVignetteFilter *disFilter = [[GPUImageVignetteFilter alloc] init];
+    //设置要渲染的区域
+    [disFilter forceProcessingAtSize:inputImage.size];
+    [disFilter useNextFrameForImageCapture];
+    
+    //添加滤镜
+    [stillImageSource addTarget:disFilter];
+    //开始渲染
     [stillImageSource processImage];
-    
-    UIImage *currentFilteredVideoFrame = [stillImageFilter imageFromCurrentFramebuffer];
-    UIImageView *imageView = [[UIImageView alloc] initWithImage:currentFilteredVideoFrame];
-    imageView.frame = self.view.bounds;
+    //获取渲染后的图片并加载到 View
+    UIImage *newImage = [disFilter imageFromCurrentFramebuffer];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:newImage];
+    imageView.frame = CGRectMake(0, 0, newImage.size.width, newImage.size.height);
+    imageView.center = self.view.center;
     [self.view addSubview:imageView];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void)didReceiveMemoryWarning {
