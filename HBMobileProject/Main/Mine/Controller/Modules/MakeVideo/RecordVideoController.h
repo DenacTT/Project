c9b35e2efa976c67be252ba8c7f2c5cf25b32108
HarbingWang <1792427320@qq.com>
2016-09-24 16:00:36 +0800

    RecordVideoController

---
+++ HBMobileProject/Main/Mine/Controller/Modules/MakeVideo/RecordVideoController.h
@@ -0,0 +1,13 @@
+//
+//  RecordVideoController.h
+//  HBMobileProject
+//
+//  Created by HarbingWang on 16/9/24.
+//  Copyright © 2016年 HarbingWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecordVideoController : UIViewController
+
+@end
