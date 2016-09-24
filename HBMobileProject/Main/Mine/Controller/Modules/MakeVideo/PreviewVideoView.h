4b4c3b5545145b910f7078091e075fffa726ea7e
HarbingWang <1792427320@qq.com>
2016-09-24 16:01:47 +0800

    PreviewVideoView

---
+++ HBMobileProject/Main/Mine/Controller/Modules/MakeVideo/PreviewVideoView.h
@@ -0,0 +1,13 @@
+//
+//  PreviewVideoView.h
+//  HBMobileProject
+//
+//  Created by HarbingWang on 16/9/24.
+//  Copyright © 2016年 HarbingWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PreviewVideoView : UIView
+
+@end
---
+++ HBMobileProject/Main/Mine/Controller/Modules/MakeVideo/PreviewVideoView.m
@@ -0,0 +1,21 @@
+//
+//  PreviewVideoView.m
+//  HBMobileProject
+//
+//  Created by HarbingWang on 16/9/24.
+//  Copyright © 2016年 HarbingWang. All rights reserved.
+//
+
+#import "PreviewVideoView.h"
+
+@implementation PreviewVideoView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
